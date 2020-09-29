6ebac0d6f56b63f0cee97a71c0360b4e7f713327
Julián Benegas <julianbenegas99@gmail.com>
2020-09-29 15:08:25 -0300

    add inter v3

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -2,6 +2,10 @@ type Props = {
   who: string
 }
 
-const Hello = ({ who }: Props) => <h1>Hello {who}!</h1>
+const Hello = ({ who }: Props) => (
+  <h1>
+    <b>Hello</b> {who}!
+  </h1>
+)
 
 export default Hello
