f1b26666a4db48cb2efda4e0a44cd7a9af95569d
Julian Benegas <julianbenegas99@gmail.com>
2021-04-01 11:44:38 -0300

    more updates

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')
+const withTM = require('next-transpile-modules')
+const withSvgr = require('next-svgr')
+
+module.exports = withPlugins(
+  [
+    withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
+    withTM([]),
+    withSvgr()
+  ],
+  { reactStrictMode: true }
+)
