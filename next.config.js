062c34a8626f03036d45a4da7689bad09f2b3bb8
Julian Benegas <julianbenegas99@gmail.com>
2021-07-21 15:24:49 -0300

    remove svgr

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 const withPlugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 const withTM = require('next-transpile-modules')
-const withSvgr = require('next-svgr')
 
 module.exports = withPlugins(
-  [
-    withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
-    withTM([]),
-    withSvgr()
-  ],
+  [withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }), withTM([])],
   { reactStrictMode: true }
 )
