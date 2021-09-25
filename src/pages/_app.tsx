ff0ce5afb0d00bb782d714fa7ba584ca082b1cc5
matiasperz <matiasperezpc@hotmail.com>
2021-09-25 16:03:28 -0300

    Adding inspect and welcome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,16 @@ import 'css/global.css'
 import { AppProps } from 'next/app'
 import { useAppGA } from 'lib/ga'
 import { AppContextProvider } from 'context/app'
+import { useMultiKeyPressTrap } from 'hooks/use-multi-key-press'
+import { isDev } from 'lib/constants'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  if (isDev) {
+    useMultiKeyPressTrap(['control+i', 'alt+i'], () =>
+      document.body.classList.toggle('inspect')
+    )
+  }
+
   useAppGA()
 
   return (
