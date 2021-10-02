3a11240b3f7f319c6ad6d684df9e879386fd91f0
Julian Benegas <julianbenegas99@gmail.com>
2021-10-02 11:09:56 -0300

    with pscale prisma

---
+++ prisma/migrations/20211002134804_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,46 @@
-import Welcome from '~/components/common/welcome'
-import { PageLayout } from '~/components/layout/page'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-const HomePage = () => {
+import { PageLayout } from '~/components/layout/page'
+import { prisma } from '~/lib/db'
+
+import { Post } from '.prisma/client'
+
+const HomePage = ({
+  posts
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <PageLayout>
-      <Welcome />
+      <h1>Posts</h1>
+      {posts.map((p) => {
+        return (
+          <div key={p.id}>
+            <h2>{p.title}</h2>
+            <p>{p.content}</p>
+          </div>
+        )
+      })}
     </PageLayout>
   )
+}
+
+export const getStaticProps: GetStaticProps<{
+  posts: Array<
+    Omit<Post, 'createdAt' | 'updatedAt'> & {
+      createdAt: string
+      updatedAt: string
+    }
+  >
+}> = async () => {
+  const rawPosts = await prisma.post.findMany()
+  return {
+    props: {
+      posts: rawPosts.map((p) => ({
+        ...p,
+        createdAt: p.createdAt.toISOString(),
+        updatedAt: p.updatedAt.toISOString()
+      }))
+    }
+  }
 }
 
 export default HomePage
