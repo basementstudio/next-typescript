ca978a981aba96e0b45d41bd2bf6c5e6683c9a12
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2021-10-06 11:56:07 -0300

    remove class when user click

--- src/hooks/use-tabbing-detect.ts
+++ src/hooks/use-tabbing-detect.ts
@@ -1,11 +1,26 @@
 import { useEffect } from 'react'
 
 // https://medium.com/hackernoon/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2
+
+const tabbingClass = 'user-is-tabbing'
+
 function handleFirstTab(event: KeyboardEvent) {
   if (event.code === `Tab`) {
-    document.body.classList.add('user-is-tabbing')
+    reset()
+    document.body.classList.add(tabbingClass)
     window.removeEventListener('keydown', handleFirstTab)
   }
+}
+
+function handleClick() {
+  reset()
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+function reset() {
+  document.body.classList.remove(tabbingClass)
+  window.removeEventListener('keydown', handleClick)
+  window.removeEventListener('mousedown', handleFirstTab as EventListener)
 }
 
 export const useTabbingDetect = () => {
@@ -13,9 +28,11 @@ export const useTabbingDetect = () => {
     if (typeof window === 'undefined') return
 
     window.addEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleClick)
 
     return () => {
       window.removeEventListener('keydown', handleFirstTab)
+      window.addEventListener('click', handleClick)
     }
   }, [])
 
