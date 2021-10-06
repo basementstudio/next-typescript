274044d1d260258b72fc03b2d5f0d8a254e1bddd
Julian Benegas <julianbenegas99@gmail.com>
2021-10-06 14:04:21 -0300

    dont remove event listeners when isTabbing changes

--- src/hooks/use-tabbing-detect.ts
+++ src/hooks/use-tabbing-detect.ts
@@ -4,32 +4,23 @@ export const useTabbingDetect = (bodyClassName = 'user-is-tabbing') => {
   const [isTabbing, setIsTabbing] = React.useState(false)
 
   React.useEffect(() => {
-    if (isTabbing) return
-
     function handleKeyDown(event: KeyboardEvent) {
       if (event.code === `Tab`) {
         setIsTabbing(true)
       }
     }
 
-    window.addEventListener('keydown', handleKeyDown)
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [isTabbing])
-
-  React.useEffect(() => {
-    if (!isTabbing) return
-
     function handleMouseDown() {
       setIsTabbing(false)
     }
 
+    window.addEventListener('keydown', handleKeyDown)
     window.addEventListener('mousedown', handleMouseDown)
     return () => {
+      window.removeEventListener('keydown', handleKeyDown)
       window.removeEventListener('mousedown', handleMouseDown)
     }
-  }, [isTabbing])
+  }, [])
 
   React.useEffect(() => {
     if (!bodyClassName) return
