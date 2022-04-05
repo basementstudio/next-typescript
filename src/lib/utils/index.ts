e602f21a6ab83b1572d5ed71eeaf2d9e2468280b
matiasperz <matiasperezpc@hotmail.com>
2022-04-04 21:00:31 -0300

    Adding get size utility

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -14,3 +14,30 @@ export const formatError = (
 }
 
 export const isApiSupported = (api: string) => isClient && api in window
+
+/* Builds responsive sizes string for images */
+export const getSizes = (
+  entries: ({ breakpoint: string; width: string } | string | number)[]
+) => {
+  const sizes = entries.map((entry) => {
+    if (!entry) {
+      return ''
+    }
+
+    if (typeof entry === 'string') {
+      return entry
+    }
+
+    if (typeof entry === 'number') {
+      return `${entry}px`
+    }
+
+    if (entry.breakpoint.includes('px')) {
+      return `(min-width: ${entry.breakpoint}) ${entry.width}`
+    }
+
+    throw new Error(`Invalid breakpoint: ${entry.breakpoint}`)
+  })
+
+  return sizes.join(', ')
+}
