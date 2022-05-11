4514e0154235f64c26577dc4ca36eb2eeeae3b77
Julian Benegas <julianbenegas99@gmail.com>
2022-05-11 13:42:27 -0300

    improve error message

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,7 +6,12 @@ export const isServer = !isClient
 
 if (typeof process.env.NEXT_PUBLIC_SITE_URL !== 'string') {
   throw new Error(
-    "Please set the NEXT_PUBLIC_SITE_URL environment variable to your site's URL."
+    `Please set the NEXT_PUBLIC_SITE_URL environment variable to your site's URL.
+    
+1. Create .env file at the root of your project.
+2. Add NEXT_PUBLIC_SITE_URL=http://localhost:3000
+3. For other environments (like production), make sure you set the correct URL.
+    `
   )
 }
 
