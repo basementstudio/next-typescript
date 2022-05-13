2b857f8a2b0d1ba6f19a9ccc2ee8e199aba7f04f
Julian Benegas <julianbenegas99@gmail.com>
2022-05-12 21:41:07 -0300

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const isDev = process.env.NODE_ENV === 'development'
 export const isProd = process.env.NODE_ENV === 'production'
 
-export const isClient = typeof window !== 'undefined'
+export const isClient = typeof document !== 'undefined'
 export const isServer = !isClient
 
 if (typeof process.env.NEXT_PUBLIC_SITE_URL !== 'string') {
