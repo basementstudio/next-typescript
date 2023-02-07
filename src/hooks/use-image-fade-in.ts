43eb35ffb53911070b54d57ec5fb4b73f87669e5
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2023-02-07 15:00:50 -0300

    lint

--- src/hooks/use-image-fade-in.ts
+++ src/hooks/use-image-fade-in.ts
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export const useImageFadeIn = () => {
   const [loaded, setLoaded] = useState(false)
   return {
-    style: { opacity: loaded ? undefined : 0},
+    style: { opacity: loaded ? undefined : 0 },
     onLoad: () => {
       setLoaded(true)
     }
