361833130d1c6b245ed79b52c2b58fe2229f7bfd
Nacho Mandagaran <33981565+ignmandagaran@users.noreply.github.com>
2023-03-29 16:02:56 -0300

    feat: polymorphic container comp (#266)

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -3,10 +3,13 @@ import * as React from 'react'
 
 export const Container = React.forwardRef<
   HTMLDivElement,
-  JSX.IntrinsicElements['div']
->(({ className, ...props }, ref) => {
+  JSX.IntrinsicElements['div'] & {
+    as?: 'div' | 'section'
+  }
+>(({ className, as = 'div', ...props }, ref) => {
+  const Element: React.ElementType = as
   return (
-    <div
+    <Element
       {...props}
       className={clsx(
         // TODO: Put some padding, max width, and margin-x auto in here!
