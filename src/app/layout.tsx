4a89a1a5b16027298892b8fcc24874d4f8d9422d
JOYBOY-0 <falamino00@gmail.com>
2023-05-10 15:09:15 -0300

    moved from page to app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import '~/css/global.scss'
+
+import { Inter } from '@next/font/google'
+import type { Metadata } from 'next'
+
+import { AppHooks } from './app-hooks'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-body' })
+
+export const metadata: Metadata = {
+  title: {
+    default: 'next-typescript | basement.studio',
+    template: '%s | basement.studio'
+  },
+  description: `A minimalist's boilerplate — Next.js with TypeScript.`,
+  //   alternates: {
+  //     canonical: isClient ? window.location.href : ''
+  //   },
+  openGraph: {
+    type: 'website',
+    siteName: 'next-typescript | basement.studio',
+    title: 'next-typescript | basement.studio'
+  },
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png'
+    }
+  ],
+  manifest: '/manifest.webmanifest',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'next-typescript | basement.studio',
+    creator: '@basementstudio',
+    siteId: '@basementstudio'
+  }
+}
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body style={{ opacity: 0 }} className={inter.variable}>
+        {children}
+        <AppHooks />
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
